181c3d8c5421f3bf59dfd3fe25b9e658be0aa41b
shunkominato <shun19871001@gmail.com>
2023-12-08 09:16:29 +0900

    e

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 import Home from './src/screen/Home';
 import SignIn from './src/screen/SignIn';
 
 const Stack = createNativeStackNavigator();
+const queryClient: QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const MyTheme = {
   ...DefaultTheme,
@@ -19,10 +33,12 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator>
-          <Stack.Screen name='SignIn' component={SignIn} />
-          <Stack.Screen name='Home' component={Home} />
-        </Stack.Navigator>
+        <QueryClientProvider client={queryClient}>
+          <Stack.Navigator>
+            <Stack.Screen name='SignIn' component={SignIn} />
+            <Stack.Screen name='Home' component={Home} />
+          </Stack.Navigator>
+        </QueryClientProvider>
       </NavigationContainer>
     </NativeBaseProvider>
   );
